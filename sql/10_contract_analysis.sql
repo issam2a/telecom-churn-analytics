69e06adf52cb9a57052bc65f6ed6680de5f4d1ef
issam <your.email@example.com>
2026-07-15 18:54:16 +0300

    add contract analysis sql file to see the contract impact on churn

---
+++ sql/10_contract_analysis.sql
@@ -0,0 +1,65 @@
+-- 1. Which contract type has highest churn?
+
+select 
+	contract ,
+	round(
+		avg(churn::int) *100.0
+	,2) as churn_rate_pct 
+from 
+	telecom.v_telecom_churn
+group by contract 
+
+order by 
+	churn_rate_pct desc ;
+
+	
+-- 2. Revenue by contract type
+select 
+	contract ,
+	sum(monthly_charges) as revenue_by_contract 
+from 
+	telecom.v_telecom_churn
+
+where churn = true 
+group by 
+	contract 
+
+order by
+	revenue_by_contract  desc
+	;
+
+-- 3. Contract distribution
+
+select 
+	contract,
+	count(*) as customers ,
+	round(
+		count(*) * 100.0 
+		/ 
+		sum(count(*)) over()
+		,2
+	) as customers_pct 
+from 
+	telecom.v_telecom_churn 
+group by 
+	contract 
+order by 
+	customers_pct 
+
+-- 4. Impact of long-term contracts
+
+select 
+	case 
+		when contract = 'Month-to-month' then 'short term'
+		else 'long term'
+	end as contract_group ,
+
+	count(*) as customers ,
+
+	round(
+		avg(churn::int) * 100.0
+	,2) as churn_rate_pct
+from 
+	telecom.v_telecom_churn 
+group by 
+	contract_group 
